d7622b63b26b6155457ae441a7627be87bd061e8
EC2 Default User <ec2-user@ip-172-31-71-119.ec2.internal>
2023-06-14 23:41:49 +0000

    140620232143

--- classes/conexao.py
+++ classes/conexao.py
@@ -4,7 +4,7 @@ def conectar():
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
-        database="aula"
+        password="senai1999",
+        database="livros"
         )
     return mydb
